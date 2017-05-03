ec3f4eff8323414b945b814ba839a7aa6d854f73
Dmytro Yarmak <dmytroyarmak@gmail.com>
2017-05-03 13:23:48 +0300

    Add slides with spec and images

--- assets/code-examples/accounts-service.spec.js
+++ assets/code-examples/accounts-service.spec.js
@@ -1,125 +1,145 @@
 import {
-    getPrimaryAccountTransactions,
-    __RewireAPI__
+  getPrimaryAccountTransactions,
+  __RewireAPI__
 } from './accounts-service';
 
 describe('getPrimaryAccountTransactions', () => {
-    let returnedPromise;
-    let getPrimaryAccountSpy;
-    let getPrimaryAccountDefer;
-    let getTransactionsSpy;
-    let getTransactionsDefer;
+  let returnedPromise;
+  let getPrimaryAccountDefer;
+  let getPrimaryAccountSpy;
+  let getTransactionsDefer;
+  let getTransactionsSpy;
 
-    beforeEach(() => {
-        // Mock getPrimaryAccount
-        getPrimaryAccountDefer = defer();
-        getPrimaryAccountSpy = jasmine.createSpy('getPrimaryAccount');
-        getPrimaryAccountSpy.and.returnValue(getPrimaryAccountDefer.promise);
-        __RewireAPI__.__Rewire__('getPrimaryAccount', getPrimaryAccountSpy);
+  beforeEach(() => {
+    // Mock getPrimaryAccount
+    getPrimaryAccountDefer = defer();
+    getPrimaryAccountSpy =
+      jasmine.createSpy('getPrimaryAccount');
+    getPrimaryAccountSpy
+      .and.returnValue(getPrimaryAccountDefer.promise);
+    __RewireAPI__.__Rewire__(
+      'getPrimaryAccount', getPrimaryAccountSpy
+    );
 
-        // Mock getTransactions
-        getTransactionsDefer = defer();
-        getTransactionsSpy = jasmine.createSpy('getTransactions');
-        getTransactionsSpy.and.returnValue(getTransactionsDefer.promise);
-        __RewireAPI__.__Rewire__('getTransactions', getTransactionsSpy);
+    // Mock getTransactions
+    getTransactionsDefer = defer();
+    getTransactionsSpy =
+      jasmine.createSpy('getTransactions');
+    getTransactionsSpy
+      .and.returnValue(getTransactionsDefer.promise);
+    __RewireAPI__.__Rewire__(
+      'getTransactions', getTransactionsSpy
+    );
 
-        returnedPromise = getPrimaryAccountTransactions();
+    returnedPromise = getPrimaryAccountTransactions();
+  });
+
+  afterEach(() => {
+    __RewireAPI__.__ResetDependency__('getPrimaryAccount');
+    __RewireAPI__.__ResetDependency__('getTransactions');
+  });
+
+  it('should call getPrimaryAccount', () => {
+    expect(getPrimaryAccountSpy).toHaveBeenCalled();
+  })
+
+  describe('when getPrimaryAccount succeed', () => {
+    let getPrimaryAccountResultFixture;
+
+    beforeEach((done) => {
+      getPrimaryAccountResultFixture = { id: 42 };
+      getPrimaryAccountDefer
+        .resolve(getPrimaryAccountResultFixture);
+      getPrimaryAccountDefer.promise.then(done);
     });
 
-    afterEach(() => {
-        __RewireAPI__.__ResetDependency__('getPrimaryAccount');
-        __RewireAPI__.__ResetDependency__('getTransactions');
+    it('should call getTransactions with id', () => {
+      expect(getTransactionsSpy)
+        .toHaveBeenCalledWith(
+          getPrimaryAccountResultFixture.id
+        );
     });
 
-    it('should call getPrimaryAccount', () => {
-        expect(getPrimaryAccountSpy).toHaveBeenCalled();
-    })
+    describe('when getTransactions succeed', () => {
+      let getTransactionsResultFixture;
 
-    describe('when getPrimaryAccount succeed', () => {
-        let getPrimaryAccountResultFixture;
+      beforeEach((done) => {
+        getTransactionsResultFixture = [
+          {id: 1, amount: 100, name: 'First transaction'},
+          {id: 2, amount: 200, name: 'Second transaction'},
+        ];
+        getTransactionsDefer
+          .resolve(getTransactionsResultFixture);
+        getTransactionsDefer.promise.then(done);
+      });
 
-        beforeEach((done) => {
-            getPrimaryAccountResultFixture = {id: 42};
-            getPrimaryAccountDefer.resolve(getPrimaryAccountResultFixture);
-            getPrimaryAccountDefer.promise.then(done);
+      describe('returned promise', () => {
+        it('should be resolved with t10s', (done) => {
+          returnedPromise.then((result) => {
+            expect(result)
+              .toBe(getTransactionsResultFixture);
+            done();
+          });
         });
-
-        it('should call getTransactions with correct id', () => {
-            expect(getTransactionsSpy).toHaveBeenCalledWith(getPrimaryAccountResultFixture.id);
-        });
-
-        describe('when getTransactions succeed', () => {
-            let getTransactionsResultFixture;
-
-            beforeEach((done) => {
-                getTransactionsResultFixture = [
-                    {id: 1, amount: 100, name: 'First transaction'},
-                    {id: 2, amount: 200, name: 'Second transaction'},
-                ];
-                getTransactionsDefer.resolve(getTransactionsResultFixture);
-                getTransactionsDefer.promise.then(done);
-            });
-
-            describe('returned promise', () => {
-                it('should be resolved with correct transactions', (done) => {
-                    returnedPromise.then((result) => {
-                        expect(result).toBe(getTransactionsResultFixture);
-                        done();
-                    });
-                });
-            });
-        });
-
-        describe('when getTransactions failed', () => {
-            let getTransactionsErrorFixture;
-
-            beforeEach((done) => {
-                getTransactionsErrorFixture = new Error();
-                getTransactionsDefer.reject(getTransactionsErrorFixture);
-                getTransactionsDefer.promise.catch(done);
-            });
-
-            describe('returned promise', () => {
-                it('should be rejected with getTransactions error', (done) => {
-                    returnedPromise.catch((error) => {
-                        expect(error).toBe(getTransactionsErrorFixture);
-                        done();
-                    })
-                });
-            });
-        });
+      });
     });
 
-    describe('when getPrimaryAccount failed', () => {
-        let getPrimaryAccountErrorFixture;
+    describe('when getTransactions failed', () => {
+      let getTransactionsErrorFixture;
 
-        beforeEach((done) => {
-            getPrimaryAccountErrorFixture = new Error();
-            getPrimaryAccountDefer.reject(getPrimaryAccountErrorFixture);
-            getPrimaryAccountDefer.promise.catch(done);
-        });
+      beforeEach((done) => {
+        getTransactionsErrorFixture = new Error();
+        getTransactionsDefer
+          .reject(getTransactionsErrorFixture);
+        getTransactionsDefer.promise.catch(done);
+      });
 
-        describe('returned promise', () => {
-            it('should be rejected with getPrimaryAccount error', (done) => {
-                returnedPromise.catch((error) => {
-                    expect(error).toBe(getPrimaryAccountErrorFixture);
-                    done();
-                })
-            });
+      describe('returned promise', () => {
+        it('should be rejected with error', (done) => {
+          returnedPromise.catch((error) => {
+            expect(error)
+              .toBe(getTransactionsErrorFixture);
+            done();
+          })
         });
+      });
     });
+  });
+
+  describe('when getPrimaryAccount failed', () => {
+    let getPrimaryAccountErrorFixture;
+
+    beforeEach((done) => {
+      getPrimaryAccountErrorFixture = new Error();
+      getPrimaryAccountDefer
+        .reject(getPrimaryAccountErrorFixture);
+      getPrimaryAccountDefer.promise.catch(done);
+    });
+
+    describe('returned promise', () => {
+      it('should be rejected with error', (done) => {
+        returnedPromise.catch((error) => {
+          expect(error)
+            .toBe(getPrimaryAccountErrorFixture);
+          done();
+        })
+      });
+    });
+  });
 });
 
 // Test helpers
 function defer() {
-    var resolve, reject;
-    var promise = new Promise(function() {
-        resolve = arguments[0];
-        reject = arguments[1];
-    });
-    return {
-        resolve: resolve,
-        reject: reject,
-        promise: promise
-    };
+  var _resolve, _reject;
+
+  var _promise = new Promise((resolve, reject) => {
+  _resolve = resolve;
+  _reject = reject;
+  });
+
+  return {
+  resolve: _resolve,
+  reject: _reject,
+  promise: _promise
+  };
 }
--- presentation/index.js
+++ presentation/index.js
@@ -10,6 +10,7 @@ import {
   ListItem,
   List,
   Quote,
+  Image,
   Slide,
   Text
 } from "spectacle";
@@ -28,17 +29,17 @@ require("spectacle/lib/themes/default/index.css");
 
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+  jasmine1: require("../assets/jasmine-1.jpg"),
+  jasmine2: require("../assets/jasmine-2.jpg"),
+  jasmine3: require("../assets/jasmine-3.jpg"),
+  jasmine4: require("../assets/jasmine-4.jpg")
 };
 
 preloader(images);
 
 const theme = createTheme({
   primary: "white",
-  secondary: "#1F2022",
+  secondary: "#4F2022",
   tertiary: "#03A9FC",
   quartenary: "#CECECE"
 }, {
@@ -58,12 +59,33 @@ export default class Presentation extends React.Component {
             How to mock functions in ES Modules?
           </Text>
         </Slide>
+        <Slide transition={["fade"]}>
+          <Heading>Babel</Heading>
+          <Image src={images.jasmine1} />
+        </Slide>
+        <Slide transition={["fade"]}>
+          <Heading>Jasmine</Heading>
+          <Image src={images.jasmine2} />
+        </Slide>
+        <Slide transition={["fade"]}>
+          <Heading>Jasmine Spy</Heading>
+          <Image src={images.jasmine3} />
+        </Slide>
+        <Slide transition={["fade"]}>
+          <Heading>Rewire</Heading>
+          <Image src={images.jasmine4} />
+        </Slide>
         <CodeSlide
           textSize="0.8em"
           lang="js"
           code={require("!raw-loader!../assets/code-examples/accounts-service")}
           ranges={[
-            { title: "accounts-service.js" },
+            { loc: [0, 0], title: "accounts-service.js" },
+
+            { loc: [2, 3] },
+            { loc: [8, 9] },
+            { loc: [14, 15] },
+
             { loc: [0, 1] },
 
             { loc: [2, 3] },
@@ -81,6 +103,139 @@ export default class Presentation extends React.Component {
             { loc: [14, 20], title: "How to unit test it?" }
           ]}
         />
+        <CodeSlide
+          textSize="0.8em"
+          lang="js"
+          code={require("!raw-loader!../assets/code-examples/accounts-service.spec")}
+          ranges={[
+            { loc: [0, 0], title: "accounts-service.spec.js" },
+            { loc: [130, 145] },
+
+            { loc: [0, 4] },
+
+            { loc: [5, 6], title: "Success flow" },
+            { loc: [41, 42] },
+            { loc: [45, 46] },
+            { loc: [55, 56] },
+            { loc: [62, 63] },
+            { loc: [75, 76] },
+            { loc: [76, 77] },
+
+            { loc: [5, 6] },
+            { loc: [6, 7] },
+            { loc: [7, 9] },
+            { loc: [9, 11] },
+
+            { loc: [12, 13] },
+
+            { loc: [14, 15] },
+            { loc: [15, 17] },
+            { loc: [17, 19] },
+            { loc: [19, 22] },
+            { loc: [13, 22] },
+
+            { loc: [24, 25] },
+            { loc: [25, 27] },
+            { loc: [27, 29] },
+            { loc: [29, 32] },
+            { loc: [23, 32] },
+
+            { loc: [33, 34] },
+
+            { loc: [36, 40] },
+
+            { loc: [41, 44] },
+
+            { loc: [45, 46] },
+            { loc: [46, 47] },
+            { loc: [48, 49] },
+            { loc: [49, 50] },
+            { loc: [50, 52] },
+            { loc: [52, 53] },
+            { loc: [45, 54] },
+
+            { loc: [55, 61] },
+
+            { loc: [62, 63] },
+            { loc: [63, 64] },
+            { loc: [65, 66] },
+            { loc: [66, 70] },
+            { loc: [70, 72] },
+            { loc: [72, 73] },
+            { loc: [62, 74] },
+
+            { loc: [75, 76] },
+            { loc: [76, 77] },
+            { loc: [77, 78] },
+            { loc: [78, 80] },
+            { loc: [80, 81] },
+            { loc: [75, 84] },
+
+            { loc: [108, 109], title: "Error flow #1" },
+
+            { loc: [5, 6] },
+            { loc: [41, 42] },
+            { loc: [108, 109] },
+            { loc: [118, 119] },
+            { loc: [119, 120] },
+
+            { loc: [108, 109] },
+            { loc: [109, 110] },
+            { loc: [111, 112] },
+            { loc: [112, 113] },
+            { loc: [113, 115] },
+            { loc: [115, 116] },
+            { loc: [108, 117] },
+
+            { loc: [118, 119] },
+            { loc: [119, 120] },
+            { loc: [120, 121] },
+            { loc: [121, 123] },
+            { loc: [123, 124] },
+            { loc: [118, 127] },
+
+
+            { loc: [86, 87], title: "Error flow #2" },
+
+            { loc: [5, 6] },
+            { loc: [41, 42] },
+            { loc: [45, 46] },
+            { loc: [55, 56] },
+            { loc: [86, 87] },
+            { loc: [96, 97] },
+            { loc: [97, 98] },
+
+            { loc: [86, 87] },
+            { loc: [87, 88] },
+            { loc: [89, 90] },
+            { loc: [90, 91] },
+            { loc: [91, 93] },
+            { loc: [93, 94] },
+            { loc: [86, 95] },
+
+            { loc: [96, 97] },
+            { loc: [97, 98] },
+            { loc: [98, 99] },
+            { loc: [99, 101] },
+            { loc: [101, 102] },
+            { loc: [96, 105] }
+          ]}
+        />
+         <CodeSlide
+          textSize="0.8em"
+          lang="js"
+          code={require("!raw-loader!../assets/code-examples/karma.conf")}
+          ranges={[
+            { loc: [0, 0], title: "karma.conf.js" },
+            { loc: [28, 31] },
+            { loc: [33, 34] },
+            { loc: [40, 46] }
+          ]}
+        />
+        <Slide>
+          <Heading size={1}>Thank you!</Heading>
+          <Heading size={2}>Questions?</Heading>
+        </Slide>
       </Deck>
     );
   }
